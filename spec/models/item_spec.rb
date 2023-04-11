f42fa3cad80fc8e3d121c985ae17d40f81024dbb
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-04-11 17:31:56 +0200

    Item tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-  it "should be valid if it has name and value"
-  it "validations should exist"
+  it "should be valid if it has name and value" do
+    item = create(:item)
+    expect(item).to be_valid
+  end
+  it "validation value should exist" do
+    item = build(:item, value: nil)
+    item.valid?
+    expect(item.errors[:value]).to include("can't be blanck")
+  end
+  it "validation name should exist" do
+    item = build(:item, name: nil)
+    item.valid?
+    expect(item.errors[:name]).to include("can't be blanck")
+  end
   it "class and instance methods should work correctly"
 end
