cd6d3ef6a6a04e976f6e66792e933335207ca0fb
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-04-11 17:11:14 +0200

    tests for item (pending)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid if it has name and value"
+  it "validations should exist"
+  it "class and instance methods should work correctly"
 end
--- spec/models/timestamp_spec.rb
+++ spec/models/timestamp_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Timestamp, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "should belong to a item"
 end
