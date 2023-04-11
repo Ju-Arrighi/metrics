f8aece146b7e0c81b5f5bdaed4e262fcab23f2ba
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-04-11 20:58:12 +0200

    adapted tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-  it "should be valid if it has name and value" do
+  it "should be valid if it has name, value and timestamp" do
     item = create(:item)
     expect(item).to be_valid
   end
@@ -15,6 +15,11 @@ RSpec.describe Item, type: :model do
     item = build(:item, name: nil)
     item.valid?
     expect(item.errors[:name]).to include("can't be blanck")
+  end
+  it "validation timestamp should exist" do
+    item = build(:item, timestamp: nil)
+    item.valid?
+    expect(item.errors[:timestamp]).to include("can't be blanck")
   end
   it "class and instance methods should work correctly"
 end
