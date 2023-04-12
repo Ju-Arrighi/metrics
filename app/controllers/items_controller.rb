b0d56f042a65f0ec1f030fa8f3d3f4f972821006
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-04-12 18:35:16 +0200

    form to post data done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
     @items = Item.all
   end
 
-  def new
-    @items = Item.new
-  end
   def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path, notice: "Metric added successfully"
+    else
+      redirect_to root_path, notice: "Invalid data, try again."
+    end
+  end
 
+  def items_params
+    params.require(:item).permit(:id)
   end
 end
